1158dd8aff1d1cb171de058332e5483294a81ccd
Zahid Khan <zkh22@uclive.ac.nz>
2022-05-23 11:05:26 +1200

    Assignement 5

---
+++ uc-seng301-wordleapp-assignment5/uc-seng301-wordleapp-assignment5/ANSWERS.md
@@ -0,0 +1,85 @@
+# SENG301 Assignment 5 (2022) - Student answers
+
+**YOUR NAME**
+
+## Task 1 - Identify the patterns in the code
+
+### Pattern 1
+
+#### What pattern is it?
+
+**YOUR ANSWER**
+
+#### What is its goal in the code?
+
+**YOUR ANSWER**
+
+#### Name of UML Class diagram attached:
+
+**YOUR ANSWER**
+
+#### Mapping to GoF pattern elements:
+
+| GoF element | Code element |
+| ----------- | ------------ |
+|             |              |
+
+
+### Pattern 2
+
+#### What pattern is it?
+
+**YOUR ANSWER**
+
+#### What is its goal in the code?
+
+**YOUR ANSWER**
+
+#### Name of UML Class diagram attached:
+
+**YOUR ANSWER**
+
+#### Mapping to GoF pattern elements:
+
+| GoF element | Code element |
+| ----------- | ------------ |
+|             |              |
+
+
+## Task 2 - Full UML Class diagram
+
+### Name of file of full UML Class diagram attached
+
+**YOUR ANSWER**
+
+
+## Task 3 - Implement new feature
+
+### What pattern fulfils the need for the feature?
+
+**YOUR ANSWER**
+
+### What is its goal and why is it needed here?
+
+**YOUR ANSWER**
+
+### Name of UML Class diagram attached:
+
+**YOUR ANSWER**
+
+### Mapping to GoF pattern elements:
+
+| GoF element | Code element |
+| ----------- | ------------ |
+|             |              |
+
+
+## Task 4 - BONUS - Acceptance tests for Task 4
+
+### Feature file (Cucumber Scenarios)
+
+**NAME OF FEATURE FILE**
+
+### Java class implementing the acceptance tests
+
+**NAME OF JAVA FILE**
---
+++ uc-seng301-wordleapp-assignment5/uc-seng301-wordleapp-assignment5/app/src/main/java/uc/seng301/wordleapp/assignment5/guesser/ManualGuesser.java
@@ -0,0 +1,82 @@
+package uc.seng301.wordleapp.assignment5.guesser;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.Stack;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import uc.seng301.wordleapp.assignment5.dictionary.DictionaryQuery;
+import uc.seng301.wordleapp.assignment5.dictionary.DictionaryResponse;
+import uc.seng301.wordleapp.assignment5.dictionary.DictionaryService;
+import uc.seng301.wordleapp.assignment5.game.ColourCoder;
+import uc.seng301.wordleapp.assignment5.game.Wordle;
+
+/**
+ * Provides guessing functionality for the command line
+ */
+public class ManualGuesser extends Guesser {
+    private static final Logger LOGGER = LogManager.getLogger(ManualGuesser.class);
+
+    private final Scanner cli;
+    private final PrintStream output;
+    private final DictionaryQuery dictionaryQuery;
+    private final ColourCoder colourCoder;
+    private String currentGuess;
+    private Stack<Guess> guesses = new Stack<>();
+
+    public ManualGuesser(Wordle wordle, Scanner cli, PrintStream output) {
+        super(wordle);
+        this.cli = cli;
+        this.output = output;
+        this.dictionaryQuery = new DictionaryService();
+        this.colourCoder = new ColourCoder(wordle);
+    }
+
+    /**
+     * Asks for and reads the next guess from the command line, also allowing for
+     * hints
+     * Returns null if the player exits the game
+     *
+     * @return Guess made with appropriate input
+     */
+    public Guess getGuess() {
+        LOGGER.info("Getting guess from command line with manual guesser");
+        String input = "";
+        boolean isPlaying = true;
+        while (isPlaying) {
+            output.println("Please enter your guess:");
+            input = cli.nextLine();
+            if (input.length() > 4 && input.substring(0, 5).equalsIgnoreCase("help ")) {
+                String query = input.split(" ")[1];
+                DictionaryResponse dictionaryResponse = dictionaryQuery.guessWord(query);
+                output.println(dictionaryResponse.getTopFiveResults());
+            } else if (input.equals("!q")) {
+                return null;
+            } else if (input.length() != 5) {
+                output.println("Guesses must be 5 letter words");
+            } else {
+                numGuesses++;
+                isPlaying = false;
+            }
+        }
+        currentGuess = input;
+        Guess guess = new GuessImpl(currentGuess, wordle);
+
+        guesses.push(guess);
+        for (Guess g : guesses) {
+            output.println(colourCoder.getColourCodedString(g.getProposition()));
+        }
+        return guess;
+    }
+
+    /**
+     * Gets the stack of guesses with more recent guesses on top
+     *
+     * @return stack of guesses
+     */
+    public Stack<Guess> getGuesses() {
+        return guesses;
+    }
+}
